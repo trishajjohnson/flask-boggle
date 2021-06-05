6fd6bea980e62ab4382632833e2f193b88740799
Trish <trishajjohnson@gmail.com>
2021-06-05 00:04:30 -0500

    final commit

--- app.py
+++ app.py
@@ -13,16 +13,30 @@ def homepage():
     print("Homepage")
     board = boggle_game.make_board()
     session["board"] = board
+    highscore = session.get("highscore", 0)
+    numPlays = session.get('numPlays', 0)
 
-    return render_template('base.html', board=board )
+    return render_template('base.html', board=board, highscore=highscore, numPlays=numPlays )
     
 
 @app.route('/check-word')
 def check_word():
     print("Print this!")
-    word = request.args["guess"]
+    guess = request.args["guess"]
+    print(guess)
     board = session["board"]
-    result = boggle_game.check_valid_word(board, word)
+    result = boggle_game.check_valid_word(board, guess)
 
     return jsonify({"result": result})
     
+
+@app.route('/post-score', methods=["POST"])
+def post_score():
+    score = request.json["score"]
+    highscore = session.get('highscore', 0)
+    numPlays = session.get('numPlays', 0)
+
+    session['numPlays'] = numPlays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(newRecord = score > highscore)
--- static/app.js
+++ static/app.js
@@ -1,56 +1,103 @@
 class BoggleGame {
     
     constructor(boardId, time=60){
-        console.log("Before the ")
+        this.score = 0;
         this.board = $('#' + boardId);
-        this.time = time;
         this.words = new Set();
         
+        this.time = time;
+        this.showTimeLeft();
+
+        this.timer = setInterval(this.countDown.bind(this), 1000);
+
         $('.guess-form', this.board).on("submit", this.handleSubmitForm.bind(this));
+        
     }
     
-    async handleSubmitForm(evt){
-        evt.preventDefault();
-        console.log("JS is this printing?")
-        const $word = $('.guess', this.board);
+    async countDown(){
+        this.time -= 1;
+        this.showTimeLeft();
 
-        let word = $word.val();
-
-        if(!word) return;
-
-        if(this.words.has(word)) {
-            return this.showMessage(`${ word } has already been found.`, 'error')
+        if(this.time === 0){
+            clearInterval(this.timer);
+            await this.scoreGame();
         }
-        
-        const response = await axios.get('/check-word', { params: { word: word } });
-
-        if(response.data.result === "ok"){
-            this.words.add(word);
-            const wordsList = $(".words-list", this.board);
-            let li = $("<li>").text(word);
-            wordsList.append(li);
-
-            return this.showMessage(`You found ${ word } on the board! Good Job!!`, 'success');
-        }
-
-        if(response.data.result === "not-on-board"){
-            return this.showMessage(`${ word } is not on the board.  Try Again!`, 'error');
-        }
-
-        if(response.data.result === "not-word"){
-            return this.showMessage(`${ word } is not a valid word.  Try Again!`, 'error');
-        }
-
-        $word.val("");
     }
 
     showMessage(msg, cls){
         $('.msg', this.board)
             .text(msg)
             .removeClass()
-            .addClass(`.msg .${cls}`);
+            .addClass(`msg ${cls}`);
     }
 
+    addWordToList(word){
+        const wordsList = $(".words-list", this.board);
+        let li = $("<li>").text(word);
+        console.log(li);
+        wordsList.append(li);
+    }
+
+    showScore(){
+        $('.score', this.board).text(this.score);
+    }
+
+    showTimeLeft(){
+        $('.time', this.board).text(this.time);
+    }
+
+    async handleSubmitForm(evt){
+        evt.preventDefault();
+        console.log("JS is this printing?")
+        const $word = $('.guess', this.board);
+        console.log($word);
+        console.log($word.val());
+        let word = $word.val();
+        console.log(word);
+        if(!word) return;
+        console.log("after first if")
+        console.log(this.words);
+        if(this.words.has(word)) {
+            this.showMessage(`'${ word }' has already been found.`, 'error');
+            return;
+        }
+        console.log("after second if statement");
+        const response = await axios.get('/check-word', { params: { guess: word } });
+        console.log(response.data.result);
+        if(response.data.result === "not-word") {
+            this.showMessage(`'${ word }' is not a valid word.  Try Again!`, 'error');
+        }
+        else if(response.data.result === "not-on-board") {
+            this.showMessage(`'${ word }' is not on the board.  Try Again!`, 'error');
+        }
+        else {
+
+            this.score += word.length;
+            this.showScore();
+
+            this.words.add(word);
+            this.addWordToList(word);
+
+            this.showMessage(`You found '${ word }' on the board! Good Job!!`, 'success');
+
+        }
+
+
+
+        $word.val("");
+    }
+
+    async scoreGame(){
+        $('.guess-form', this.board).hide()
+
+        const response = await axios.post('/post-score', { score: this.score });
+        if(response.data.newRecord){
+            this.showMessage(`New Record: ${ this.score }`, 'success');
+        }
+        else {
+            this.showMessage(`Final Score: ${ this.score }`, 'success');
+        }
+    }
 
 }
 
